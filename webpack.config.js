189d27f842de6b4581e7958ff5117f6990c2c657
Blaine Schmeisser <blainesch@gmail.com>
2016-11-27 02:25:59 -0800

    Use production builds.

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,11 @@ module.exports = {
     ],
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
         warnings: false,
