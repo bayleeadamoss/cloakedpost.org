0956a854ef988bccfa173884ece055fce588dae6
Blaine Schmeisser <blainesch@gmail.com>
2016-12-03 01:14:10 -0800

    Test.

---
+++ test.js
@@ -0,0 +1,15 @@
+var Handlebars = require('handlebars')
+var fs = require('fs')
+
+var source = fs.readFileSync('./config/load-balancer/nginx.conf.handlebars').toString()
+
+var template = Handlebars.compile(source)
+
+var data = {
+  servers: [
+    { public_ip: '138.197.21.140', name: 'app01' },
+    { public_ip: '104.236.209.164', name: 'app02' },
+  ],
+}
+
+console.log(template(data))
